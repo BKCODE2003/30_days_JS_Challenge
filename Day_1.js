e39d8a89af7b508b6d6816b158f0fec97e3935b9
BKCODE2003 <bhushankor12345.9@gmail.com>
2024-07-13 22:31:12 +0530

    Sucessfully completed Day 1 challenge 13/07/2024 at 10:30 PM

---
+++ Day_1.js
@@ -0,0 +1,66 @@
+// <--Activity 1-->
+
+    // Task 1
+
+        var num=10;
+        console.log("Number :",num)
+
+    // Task 2
+
+        let myString="Hello How are You ?"
+        console.log(myString);
+
+// <--Activity 2-->
+
+    // Task 3
+
+        const answer=true
+        console.log(answer);
+
+// <--Activity 3-->
+
+    // Task 4
+
+        var number=10;
+        var string="Hello"
+        var bool=true
+        var object={
+            name:"Bhushan"
+        }
+        var array=[1,2,3,4,5,6]
+
+        console.log(`My type is ${typeof(number)} and value is ${number}`)
+        console.log(`My type is ${typeof(string)} and value is ${string}`)
+        console.log(`My type is ${typeof(bool)} and value is ${bool}`)
+        console.log(`My type is ${typeof(object)} and value is ${object}`,object)  // see output which is ==== My type is object and value is [object Object] { name: 'Bhushan' }     why  [object Object] instead of { name: 'Bhushan' } in backtiks
+        console.log(`My type is ${typeof(array)} and value is ${array}`)
+
+// <--Activity 4-->
+
+        // Task 5
+
+            let value=10
+            console.log("Initial Value :",value)
+
+            value=100
+            console.log("Value after reassign :",value)
+
+// <--Activity 5-->
+
+        // Task 6
+            
+            let change_me_with_let="Happy Day"
+            console.log(change_me_with_let);
+
+            change_me_with_let="Happy Morning"
+            console.log(change_me_with_let);
+
+
+            const change_me="Happy Day"
+            console.log(change_me);
+
+            // change_me="Happy Morning"
+            // console.log(change_me);
+
+            //Constants can not be changed
+            
