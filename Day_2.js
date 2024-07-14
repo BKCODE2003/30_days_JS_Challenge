a1adb483113afe99b83c3a6b7ea4fcff5339ee4d
BKCODE2003 <bhushankor12345.9@gmail.com>
2024-07-14 16:33:26 +0530

    Sucessfully Completed Day 2

---
+++ Day_2.js
@@ -0,0 +1,57 @@
+//  Activity 1
+
+        var a=10,b=20
+    //Task 1
+        console.log("Result a+b :",a+b)
+    //Task 2
+        console.log("Result a-b :",a-b)
+    //Task 3
+        console.log("Result a*b :",a*b)
+    //Task 4
+        console.log("Result a/b :",a/b)
+    //Task 5
+        console.log("Result a%b :",a%b)
+
+//  Activity 2
+
+   
+        var c=10
+     //Task 6
+        c+=20
+        console.log("Result of c=10 after +=20 :",c)
+     //Task 7
+        c-=10
+        console.log("Result of c=30 after -=10 :",c) 
+
+//  Activity 3
+
+    //Task 8
+        var d=55,e=95
+        console.log("D>E :",d>e)
+        console.log("D<E :",d<e)
+    //Task 9
+        var f=100,g=100,h=50
+        console.log("F>=G :",f>=g)
+        console.log("G<=h :",g<=h)
+    //Task 10
+        var num=10
+        var mystr="10"
+        console.log("num==mystr :",num==mystr)
+        console.log("num===mystr :",num===mystr)
+
+//  Activity 4
+
+    //Task 11
+        console.log("num==10 && num!==mystr :",num==10 && num!==mystr)
+    //Task 12
+        console.log("num!=10 || num===mystr :",num==10 && num===mystr)
+    //Task13 
+        var myDecision=true
+        console.log("Negation of myDecision i.e !myDecision :",!myDecision)
+
+//  Activity 5
+
+    //Task 14
+        var num2=15;
+        var myResult=(Math.abs(num2)==num2 ? "Positive":"Negative")
+        console.log("Number in num2 is :",myResult)
